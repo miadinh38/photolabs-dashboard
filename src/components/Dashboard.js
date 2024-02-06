07b66cb2e2195efcc850ed46c9df6f2388dae4fc
Mia Dinh <miadinh38@gmail.com>
2024-02-06 00:19:17 -0500

    Fetch data from photolabs-api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,45 +3,98 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+  } from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
 class Dashboard extends Component {
-  state = { loading: false };
+  state = { 
+    loading: true,
+    focused: null,
+    photos: [],
+    topics: [],
+  };
 
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  componentDidMount() {
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(response => response.json()));
 
+    Promise.all(urlsPromise)
+    .then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics
+      });
+    });
+
+    // Retrieve the value stored in the 'focused' key of localStorage
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    // If a value is found in localStorage, update the component state with it
+    if(focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // Check if the 'focused' state has changed
+    if (prevState.focused !== this.state.focused) {
+      // If changed, update the 'focused' value in localStorage
+      localStorage.setItem("focused", JSON.stringify(this.state.focused))
+    }
+  }
+
+  selectPanel(id) {
+    this.setState(prevState => ({
+      focused: prevState.focused != null ? null : id
+    }));
+  }
+
+  render() {    
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+    
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map((panel) => (
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map((panel) => (
       <Panel
       key={panel.id}
       label={panel.label}
-      value={panel.value}
+      value={panel.getValue(this.state)}
+      onSelect={event => this.selectPanel(panel.id)}
       />
     ));
 
